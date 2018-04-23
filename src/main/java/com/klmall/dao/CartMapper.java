af462a3097d7226dbeaee63a426e876a6c57296c
lukang <lukang287@163.com>
2018-04-24 00:11:02 +0800

    project init

---
+++ src/main/java/com/klmall/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.klmall.dao;
+
+import com.klmall.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
